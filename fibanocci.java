21f94ac01e870eda5e234f3e446754ecff96c7b9
Subhojyoti Majumder <64296932+subhoweb@users.noreply.github.com>
2021-06-20 10:52:51 +0530

    Add files via upload

---
+++ fibanocci.java
@@ -0,0 +1,23 @@
+//Fibonacci Series up to n terms
+
+import java.util.*;
+public class fibanocci {
+
+	public static void main(String[] args) {
+		int n, i, n1=0, n2=1,next;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input Number of Digits.. ");
+        n = sc.nextInt();
+        for(i=1; i<=n; i++){
+            System.out.print(n1 + " ");
+            next = n1+n2;
+            n1=n2;
+            n2=next;
+            
+        }
+        sc.close();
+        
+
+	}
+
+}
