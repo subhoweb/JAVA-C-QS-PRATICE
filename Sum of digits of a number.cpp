3538743d564a4df0a0b3e312d0216574ebf3112e
Subhojyoti Majumder <64296932+subhoweb@users.noreply.github.com>
2021-08-14 14:35:06 +0530

    Update Sum of digits of a number.cpp

--- Sum of digits of a number.cpp
+++ Sum of digits of a number.cpp
@@ -1,15 +1,16 @@
 #include <stdio.h>
 int main() {
-    long n = 358596;
+    long n = 3510659;
     int sum = 0;
     int i,m;
-	do
+	
+	while(n!=0)
 	{   
 		m = n%10;
 		sum = sum + m;
 		n = n/10;
 		
-	}while(i < n);
+	}
 	
 	printf("The addition is %d",sum);
 }
